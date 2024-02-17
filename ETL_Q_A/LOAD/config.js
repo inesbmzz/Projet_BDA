459ef650dbbebcc8643342dc06eeef1d133fd95d
Chakib <mcbourzag@gmail.com>
2024-02-17 16:13:42 +0100

    added testing

--- ETL_Q_A/LOAD/config.js
+++ ETL_Q_A/LOAD/config.js
@@ -4,9 +4,11 @@ dotenv.config();
 const db_uri = process.env.DBURI;
 const db_username = process.env.USERNAME;
 const db_password = process.env.PASSWORD;
+const db_name = process.env.DBNAME;
 
 module.exports = {
     db_uri,
     db_username,
-    db_password
+    db_password,
+    db_name
 }
--- ETL_Q_A/LOAD/db.js
+++ ETL_Q_A/LOAD/db.js
@@ -1,36 +1,7 @@
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-const {db_uri} = require('./config');
-uri = db_uri;
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
-
-// dbConfig.js
+const {db_uri, db_name} = require('./config');
 const { MongoClient } = require('mongodb');
 
-// const url = uri;
-const dbName = 'helloWorld';
-
-const client = new MongoClient(uri);
+const client = new MongoClient(db_uri);
 
 module.exports = {
   connect: async function () {
@@ -42,7 +13,7 @@ module.exports = {
     }
   },
   getDb: function () {
-    return client.db(dbName);
+    return client.db(db_name);
   },
 };
 
